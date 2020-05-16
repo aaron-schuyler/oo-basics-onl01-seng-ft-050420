b0603add94a3cfbeb4c82884f8963c2eea6d9098
Aaron Schuyler <asky28@gmail.com>
2020-05-16 22:42:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,10 @@
 class Book
-  attr_accessor :title, :author, :page_count, :genre, :turn_page
+  attr_accessor :title, :author, :page_count, :genre
   def initialize(title)
     @title = title
+  end
+  def turn_page
+    @page_count+=1
   end
 end
 
